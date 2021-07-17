b33ef6f1bc23d517d2638204a7481c77d75e1484
Reut Tzadok <reut.b1212@gmail.com>
2021-07-17 22:35:30 +0300

    consumer works - read messages from topic, convert to Alert and send notification via email.

--- src/main/java/com/notification/models/Alert.scala
+++ src/main/java/com/notification/models/Alert.scala
@@ -3,12 +3,13 @@ package com.notification.models
 import java.sql.Timestamp
 
 //todo use Broker - wait for Ella
+//todo change timestamp's type
 case class Alert(
                 brokerId: String,
                 brokerFirstName: String,
                 brokerLastName: String,
                 brokerCompany: String,
                 communicationTypeDetails: CommunicationDetails,
-                timeStamp: Timestamp,
+                timeStamp: String,
                 stockInfo: StockInfo
                 )
