6e36ffb24b3a5c32692db3101535b538944062d9
Reut Tzadok <reut.b1212@gmail.com>
2021-07-18 12:58:39 +0300

    with notification's message

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 Before running this project please follow behind steps:
     For cloud connection:
         * change the ip in application.yml
-        * add the ip and host name to C:\Windows\System32\drivers\etc\hosts
+        * add the ip and hostname to C:\Windows\System32\drivers\etc\hosts
 
     For email connection:
-        Add a file named "password.txt" in Data. This file should contain the sender's email address in the first line and
+        Add a file named "passwords.txt" in Data folder. This file should contain the sender's email address in the first line and
         the sender's email password in the second line.
         example:
         xxx@gmail.com
--- src/main/java/com/notification/services/AlertHandler.scala
+++ src/main/java/com/notification/services/AlertHandler.scala
@@ -1,6 +1,6 @@
 package com.notification.services
 
-import com.notification.models.Alert
+import com.notification.models.{Alert, StockInfo}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -14,8 +14,12 @@ class AlertHandler {
   def handle(alert: Alert): Unit = {
     //todo manage the notifications options
 
-    //todo create the notification content
-    val content: String = alert.stockInfo.stockTicker + alert.stockInfo.currentPrice
+    //todo add "canceled" option
+    val content = alert.stockInfo match {
+      case StockInfo(stockTicker, _, "BUY", actionRequestedAmount) => s"The price of $stockTicker stock is lower than $actionRequestedAmount"
+      case StockInfo(stockTicker, _, "SELL", actionRequestedAmount) => s"The price of $stockTicker stock is higher than $actionRequestedAmount"
+    }
+
     sendNotification.send(alert.communicationTypeDetails.communicationDetails, content)
 
   }
